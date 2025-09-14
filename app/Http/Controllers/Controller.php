9c13b82e8a10b4766d333336969ef93d7d74c28b
Omid <mehrabiomid8282@gmail.com>
2025-09-14 23:53:59 +0200

    improve: giving a automatic dynamic name to see the router changes as you go through the links

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+
 abstract class Controller
 {
-    //
+    // This is where you pass the page name dynamically
+    public function index()
+    {
+        return Inertia::render('Home', [
+            'pageName' => ucfirst(request()->route()->getName()),
+        ]);
+    }
 }
