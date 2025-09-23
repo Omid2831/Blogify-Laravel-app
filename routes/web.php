2312c6abea6101565fc9a30e0db502210be66867
Omid <mehrabiomid8282@gmail.com>
2025-09-23 22:09:23 +0200

    refactor(project): Starting with just laravel framework

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Homepage 
-Route::get('/',[])->name('homepage');
-
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+Route::get('/', function () {
+    return view('welcome');
+});
