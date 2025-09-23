85e2c2ce253e64dc9b96480ceacdaab04a69a52d
Omid <mehrabiomid8282@gmail.com>
2025-09-23 21:29:37 +0200

    refactor(frameworks): remove React to focus on learning Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,9 @@
 <?php
 
-use App\Http\Controllers\Headers\AboutController;
-use App\Http\Controllers\Homepage\HomeController;
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 // Homepage 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-// About page 
-Route::get('/About', [AboutController::class, 'index'])->name('about');
-
-// Contact page 
-Route::get('/contact', function () {
-    return "<h2 class='text-center'>You ate a lot</h2>";
-});
-
-// page webpage
-Route::get('/page', [PageController::class, 'index']);
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+Route::get('/',[])->name('homepage');
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
