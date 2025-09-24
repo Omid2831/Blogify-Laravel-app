0d452c9c985e6f70cc20e8a7f1ad0facf4ff899b
Omid <mehrabiomid8282@gmail.com>
2025-09-24 10:29:25 +0200

    improve(route): Enhanced the structure of the routing

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 // Homepage
+Route::get('/', [HomepageController::class, 'index'])->name('homepage');
 
-Route::get('/',[HomepageController::class, 'index'])->name('homepage');
+// About
+Route::get('/about', function () {
+    return view('/about');
+})->name('about');
+
+// Contact 
+Route::get('/contact', function(){
+    return view('/contact');
+})->name('contact');
