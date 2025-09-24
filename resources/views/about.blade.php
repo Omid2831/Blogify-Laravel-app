fe443dac5858ffd14e69687d6ca71ac0357d1ef8
Omid <mehrabiomid8282@gmail.com>
2025-09-24 10:31:20 +0200

    improve(view): the naming has been changed to a better state

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+<h1 class="text-warning bg-danger text-center mt-4">
+    There is nothing on the about at the moment
+</h1>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+        <h1 class="text-warning bg-danger text-center mt-4">
+            There is nothing on the contact at the moment
+        </h1>
+@endsection
