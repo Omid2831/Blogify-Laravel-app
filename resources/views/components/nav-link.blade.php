3ac267a838a3912cde31d18b0f61fdd2308db37f
Omid <mehrabiomid8282@gmail.com>
2025-09-29 23:10:23 +0200

    feat(nav-link): add support for button type and improve class handling

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,21 @@
-@props(['active' => false])
-<a class="{{ $active ? 'bg => false-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
-    aria-current="{{ $active ? 'page' : 'false' }}"
-    {{ $attributes }}
-    >
-    {{ $slot }}
-</a>
+@props(['active' => false, 'type' => 'a'])
+
+@php
+    if ($type === 'a') {
+        $classes = $active ?? false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
+    } else {
+        $classes = $active ?? false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
+    }
+@endphp
+
+@if ($type === 'a')
+    <a class="{{ $classes }} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
+        {{ $slot }}
+    </a>
+@else
+    <button class="{{ $classes }} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
+        {{ $slot }}
+    </button>
+@endif
