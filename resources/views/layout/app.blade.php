2a6819da3ebb976e056b056630f563ec3bb8f409
Omid <mehrabiomid8282@gmail.com>
2025-09-29 23:10:41 +0200

    fix(layout): restore default title and enhance body and main styles for better layout structure

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,18 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $meta['title'] ?? 'you got an error' }}</title>
+    <title>{{ $meta['title'] ?? 'Blogify - Laravel Application' }}</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4.1"></script>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
 </head>
 
-<body class="bg-gray-50 text-gray-800">
+<body class="bg-gray-50 text-gray-800 min-h-screen flex flex-col">
 
     <!-- Header -->
     <x-header />
 
     <!-- Page content -->
-    <main class="p-6">
+    <main class="flex-1">
         @yield('content')
     </main>
 
