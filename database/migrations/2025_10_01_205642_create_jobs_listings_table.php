a4ce1b07fd3c1a37e67fa2723394b8de77c0c070
Omid <mehrabiomid8282@gmail.com>
2025-10-03 00:01:21 +0200

    refactor(migration): update foreign key definition for employer_id in jobs-listings table

--- database/migrations/2025_10_01_205642_create_jobs_listings_table.php
+++ database/migrations/2025_10_01_205642_create_jobs_listings_table.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('jobs-listings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('employer_id')->constrained('employers')->onDelete('cascade');
+            $table->foreignIdFor(Employer::class)->constrained('employers')->onDelete('cascade');
             $table->string('title');
             $table->string('company');
             $table->string('location');
