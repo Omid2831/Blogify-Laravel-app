ff901cd5f16e8b623c6d7b998271dd3d8d67863e
Omid <mehrabiomid8282@gmail.com>
2025-10-05 18:37:40 +0200

    refactor(jobs): reorganize includes for better structure in index view

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -5,7 +5,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
             @include('jobs.partials.index.header')
-
+            @include('jobs.partials.searchbar')
             <!-- Jobs Grid -->
             <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                 @foreach ($jobs as $job)
@@ -19,3 +19,7 @@
 
     @include('jobs.partials.index.job-modal')
 @endsection
+
+@push('scripts')
+     @vite('resources/js/javascript.js')
+@endpush
