4ced2ba2c2d0f3c0f0187cee2e472c52c523151b
Omid <mehrabiomid8282@gmail.com>
2025-10-15 22:45:55 +0200

    feat(EmployerFactory.php): define the model for the EmployerFactory class

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -3,12 +3,16 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Employer;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
  */
 class EmployerFactory extends Factory
 {
+
+    protected $model = Employer::class;
+
     /**
      * Define the model's default state.
      *
